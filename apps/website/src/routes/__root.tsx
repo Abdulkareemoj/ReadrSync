ea0f343a1b876dc4efa3bc40b84454170ace63ae
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2026-08-05 14:13:04 +0100

    feat: wrap main route in ThemeProvider for improved theming support

--- apps/website/src/routes/__root.tsx
+++ apps/website/src/routes/__root.tsx
@@ -4,10 +4,16 @@ import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 
 import Header from "@/components/header";
 import { StickyFooter } from "@/components/sticky-footer";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export const Route = createRootRoute({
 	component: () => (
-		<>
+		<ThemeProvider
+			attribute="class"
+			enableSystem
+			disableTransitionOnChange
+			defaultTheme="system"
+		>
 			<Header />
 			<Outlet />
 			<TanStackDevtools
@@ -22,6 +28,6 @@ export const Route = createRootRoute({
 				]}
 			/>
 			<StickyFooter />
-		</>
+		</ThemeProvider>
 	),
 });
